e88a902d68ad8038a60577d85a97c1bb7240fbd9
Maxim <maksukh@inbox.ru>
2022-04-21 02:18:50 +0300

    Rework custom order and config

--- src/test/kotlin/CustomOrder.kt
+++ src/test/kotlin/CustomOrder.kt
@@ -1,17 +1,36 @@
 package org.jetbrains.teamcity.testPrioritization
 
+import org.junit.jupiter.api.ClassOrderer
+import org.junit.jupiter.api.ClassOrdererContext
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.MethodOrdererContext
+import java.lang.reflect.Method
 
-const val METHOD_CONFIG = "/test-prioritization-method-config.txt"
+const val TEST_PRIORITIZATION_CONFIG = "/test-prioritization-config.txt"
 
-class CustomOrder : MethodOrderer {
+class CustomOrder : MethodOrderer, ClassOrderer {
+    private val config = this::class.java.getResourceAsStream(TEST_PRIORITIZATION_CONFIG)
+
+    private val successProbability = config?.bufferedReader()?.readLines()?.associate {
+        it.substringBeforeLast(":") to it.substringAfterLast(":").toDouble()
+    }
+
+    private val Method.qualifiedName
+        get() = "${declaringClass.name}.${name}"
+
     override fun orderMethods(context: MethodOrdererContext) {
-        val config = this::class.java.getResourceAsStream(METHOD_CONFIG) ?: return
-        val priority = config.bufferedReader().readLines().mapIndexed { i, name -> name to i }.toMap()
+        successProbability ?: return
         context.methodDescriptors.sortBy {
-            val name = "${it.method.declaringClass.name}.${it.method.name}"
-            priority.getOrDefault(name, -1)
+            successProbability.getOrDefault(it.method.qualifiedName, 0.0)
+        }
+    }
+
+    override fun orderClasses(context: ClassOrdererContext) {
+        successProbability ?: return
+        context.classDescriptors.sortByDescending {
+            it.testClass.declaredMethods.sumOf { method ->
+                1.0 - successProbability.getOrDefault(method.qualifiedName, 0.0)
+            }
         }
     }
 }
